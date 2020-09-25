f4eadf7911c5d781145ac49e3b7ff26f99fdbc36
Pandaxubo <55469347+Pandaxubo@users.noreply.github.com>
2020-09-24 20:43:45 -0400

    面试复盘

---
+++ src/Pingan/CircularQueue.java
@@ -0,0 +1,110 @@
+package Pingan;
+
+/**
+ * @author pandaxubo
+ * @description leetcode 622
+ *
+ * 设计你的循环队列实现。 循环队列是一种线性数据结构，其操作表现基于 FIFO（先进先出）原则并且队尾被连接在队首之后以形成一个循环。它也被称为“环形缓冲器”。
+ *
+ * 循环队列的一个好处是我们可以利用这个队列之前用过的空间。在一个普通队列里，一旦一个队列满了，我们就不能插入下一个元素，即使在队列前面仍有空间。但是使用循环队列，我们能使用这些空间去存储新的值。
+ *
+ * 你的实现应该支持如下操作：
+ *
+ * MyCircularQueue(k): 构造器，设置队列长度为 k 。
+ * Front: 从队首获取元素。如果队列为空，返回 -1 。
+ * Rear: 获取队尾元素。如果队列为空，返回 -1 。
+ * enQueue(value): 向循环队列插入一个元素。如果成功插入则返回真。
+ * deQueue(): 从循环队列中删除一个元素。如果成功删除则返回真。
+ * isEmpty(): 检查循环队列是否为空。
+ * isFull(): 检查循环队列是否已满。
+ *
+ * 示例：
+ *
+ * MyCircularQueue circularQueue = new MyCircularQueue(3); // 设置长度为 3
+ * circularQueue.enQueue(1); // 返回 true
+ * circularQueue.enQueue(2); // 返回 true
+ * circularQueue.enQueue(3); // 返回 true
+ * circularQueue.enQueue(4); // 返回 false，队列已满
+ * circularQueue.Rear(); // 返回 3
+ * circularQueue.isFull(); // 返回 true
+ * circularQueue.deQueue(); // 返回 true
+ * circularQueue.enQueue(4); // 返回 true
+ * circularQueue.Rear(); // 返回 4
+ *
+ *
+ * 提示：
+ *
+ * 所有的值都在 0 至 1000 的范围内；
+ * 操作数将在 1 至 1000 的范围内；
+ * 请不要使用内置的队列库。
+ *
+ */
+public class CircularQueue {
+
+    public static void main(String[] args) {
+        MyQueue q = new MyQueue(6);
+        System.out.println("Q is empty : "+q.isEmpty());
+        System.out.println("Enqueue: "+ 4 +" "+ q.enQueue(4));
+        System.out.println("Enqueue: "+ 212 +" "+ q.enQueue(212));
+        System.out.println("Enqueue: "+ 224 +" "+ q.enQueue(224));
+        System.out.println("Enqueue: "+ 433 +" "+ q.enQueue(433));
+        System.out.println("Enqueue: "+ 446 +" "+ q.enQueue(446));
+        System.out.println("Enqueue: "+ 475 +" "+ q.enQueue(475));
+        System.out.println("Q is full : "+q.isFull());
+        System.out.println("Enqueue: "+ 4722225 +" "+ q.enQueue(4722225));
+        System.out.println("Dequeue: "+" "+ q.deQueue());
+        System.out.println("Q is full : "+q.isFull());
+        System.out.println("Enqueue: "+ 4722225 +" "+ q.enQueue(4722225));
+        System.out.println("Q is full : "+q.isFull());
+        System.out.println("Front: "+ q.Front());
+        System.out.println("Rear: "+ q.Rear());
+    }
+
+    static class MyQueue {
+        int[] queue;
+        int cap;
+        int count;
+        int head;
+
+        public MyQueue(int k) {
+            this.queue = new int[k];
+            this.cap = k;
+            this.count = 0;
+            this.head = 0;
+        }
+
+        public boolean enQueue(int value) {
+            if (this.cap == this.count) return false;
+            int tail = (this.head + this.count) % this.cap;
+            this.queue[tail] = value;
+            this.count++;
+            return true;
+        }
+
+        public boolean deQueue() {
+            if (this.count == 0) return false;
+            this.head = (this.head + 1) % this.count;
+            this.count--;
+            return true;
+        }
+
+        public int Front() {
+            if (this.count == 0) return -1;
+            return this.queue[head];
+        }
+
+        public int Rear() {
+            if (this.count == 0) return -1;
+            int tail = (this.head + this.count - 1) % this.cap;
+            return this.queue[tail];
+        }
+
+        public boolean isEmpty() {
+            return (this.count == 0);
+        }
+
+        public boolean isFull() {
+            return (this.count == this.cap);
+        }
+    }
+}
--- src/Shopee/OperateString.java
+++ src/Shopee/OperateString.java
@@ -5,14 +5,16 @@ public class OperateString {
     public static void main(String[] args) {
         String s1 = "aeasaeseaseasesasesa";
         Queue<Character> q = new PriorityQueue<>((o1,o2)->Character.compare(o2,o1));
+        Stack<Character> res = new Stack<>();
         for(int i =0; i<s1.length();i++){
             char c = s1.charAt(i);
             if(!q.contains(c)){
                 q.offer(c);
+                res.push(c);
             }
         }
         for(int j =0; j<q.size();j++){
-            System.out.print(q.poll());
+            System.out.print(res.pop());
         }
     }
 }
